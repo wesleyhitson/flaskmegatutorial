353e6f31d1caf83dfaf5befe53a6e56dee6f6d1e
Wesley Hitson <wesleyhitson106@gmail.com>
2018-06-17 19:37:19 -0400

    Halfway through ch16

---
+++ app/search.py
@@ -0,0 +1,30 @@
+"""
+All Elasticsearch code goes here. The functions defined here access the
+indices, so to swap out Elasticsearch, these are the only functions that need
+to be rewritten.
+"""
+from flask import current_app
+
+def add_to_index(index, model):
+    if not current_app.elasticsearch:
+        return
+    payload = {}
+    for filed in model.__searchable__:
+        payload[field] = getattr(model, field)
+    current_app.elasticsearch.index(index=index, doc_type=index, id=model.id,
+                                    body=payload)
+
+def remove_from_index(index, model):
+    if not current_app.elasticsearch:
+        return
+    current_app.elasticsearch.delete(index=index, doc_type=index, id=model.id)
+
+def query_index(index, query, page, per_page):
+    if not current_app.elasticsearch:
+        return [], 0
+    search = current_app.elasticsearch.search(
+        index=index, doc_type=index,
+        body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
+              'from': (page - 1) * per_page, 'size': per_page})
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    return ids, search['hits']['total']
