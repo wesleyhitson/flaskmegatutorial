764f4873e3e811e93d6841947c26065a01b04f0c
Wesley Hitson <wesleyhitson106@gmail.com>
2018-07-04 13:43:50 -0400

    Actually fix reser password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
